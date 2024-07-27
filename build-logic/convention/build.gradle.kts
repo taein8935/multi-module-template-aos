bd642354553b1caa8cd62a792166ffbb984d8f53
taein8935 <tikim8935@gmail.com>
2024-07-27 21:23:22 +0900

    Set up build-logic

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+   `kotlin-dsl`
+}
+
+group = "com.multi.module.buildlogic"
+
+dependencies {
+   compileOnly(libs.android.gradlePlugin)
+   compileOnly(libs.android.tools.common)
+   compileOnly(libs.kotlin.gradlePlugin)
+   compileOnly(libs.ksp.gradlePlugin)
+   compileOnly(libs.room.gradlePlugin)
+}
+
---
+++ build-logic/convention/src/main/java/com/multi/module/convention/MyClass.kt
@@ -0,0 +1,4 @@
+package com.multi.module.convention
+
+class MyClass {
+}
